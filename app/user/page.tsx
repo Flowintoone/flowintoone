086df3ad6b0fbcb6e7a6f4ee75af8cb54f1055ff
Datchani <d.imnoy@tlco.it>
2025-11-06 17:22:46 +0100

    modifile files

---
+++ app/user/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default async function UsersProfile() {
+
+  interface Users{
+    id : number,
+    name : string
+  }
+
+  const rest = await fetch('https://jsonplaceholder.typicode.com/users', {
+    cache : 'no-store'
+  })
+  const users: Users[] = await rest.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map(user => <li key={user.id}>name {user.name}</li>)}
+      </ul>
+    </>
+  )
+}
